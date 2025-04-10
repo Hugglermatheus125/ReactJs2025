4fb9f76bb02b748b0a16d828f4a26b6d7c04ce2b
Spani <matheushuggler125@gmail.com>
2025-04-10 17:48:45 +0000

    Aula 10/04/2025

---
+++ src/componentes/body.jsx
@@ -0,0 +1,7 @@
+export default function Body(){
+    return (
+        <div>
+            <h1>Corpo da Página</h1>
+        </div>
+    )
+}
---
+++ src/componentes/galery.jsx
@@ -0,0 +1,24 @@
+export default function Galery(){
+    return (
+        <div>
+            <h1>Corpo da Página</h1>
+            <div className="container-fluid">
+                <div className="image col-3">
+                    <img src="https://cdn.autopapo.com.br/box/uploads/2023/12/11161337/volkswagen-fusca-azul-foto.png"/>
+                </div>
+
+                <div className="image col-3">
+                    <img src="https://cdn.autopapo.com.br/box/uploads/2023/12/11161337/volkswagen-fusca-azul-foto.png"/>
+                </div>
+
+                <div className="image col-3">
+                    <img src="https://cdn.autopapo.com.br/box/uploads/2023/12/11161337/volkswagen-fusca-azul-foto.png"/>
+                </div>
+
+                <div className="image col-3">
+                    <img src="https://cdn.autopapo.com.br/box/uploads/2023/12/11161337/volkswagen-fusca-azul-foto.png"/>
+                </div>
+            </div>
+        </div>
+    )
+}
